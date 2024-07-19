64b03e82fbe7329e044ee711247cd0159ffb2b8a
[greenClothesZelda] <[wlstn36973697@gmail.com]>
2024-07-19 14:52:49 +0900

    lecture 2.1

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -14,8 +12,6 @@ import com.example.demo.answer.AnswerRepository;
 import com.example.demo.question.Question;
 import com.example.demo.question.QuestionRepository;
 
-import jakarta.transaction.Transactional;
-
 @SpringBootTest
 class DemoApplicationTests {
 
@@ -25,21 +21,20 @@ class DemoApplicationTests {
     @Autowired
     private AnswerRepository answerRepository;
 
-    @Transactional
 	@Test
     void testJpa(){
         //this.questionRepository.deleteAll();
 
         //1번
         // Question q1 = new Question();
-        // q1.setSubject("sbb는 무엇인가요?");
-        // q1.setContent("sbb에 대해 알고싶습니다.");
+        // q1.setSubject("시발");
+        // q1.setContent("왜");
         // q1.setCreateDate(LocalDateTime.now());
         // this.questionRepository.save(q1);
 
         // Question q2 = new Question();
-        // q2.setSubject("스프링부트 모델 질문입니다.");
-        // q2.setContent("id는 자동으로 생성되나요?");
+        // q2.setSubject("안되냐고");
+        // q2.setContent("대답해라");
         // q2.setCreateDate(LocalDateTime.now());
         // this.questionRepository.save(q2);
 
@@ -101,14 +96,28 @@ class DemoApplicationTests {
         // this.answerRepository.save(ans);
 
         //10번
-        Optional<Question> oq = this.questionRepository.findById(12);
-        assertTrue(oq.isPresent());
+        // Optional<Question> oq = this.questionRepository.findById(12);
+        // assertTrue(oq.isPresent());
+        // Question q = oq.get();
+
+        // List<Answer> alist = q.getAnswerList();
+
+        // assertEquals(1, alist.size());
+        // assertEquals("ap메이지만 하는 틀딱이라서", alist.get(0).getContent());
+
+        //궁금증
+        Optional<Question> oq = this.questionRepository.findById(58);
         Question q = oq.get();
 
-        List<Answer> alist = q.getAnswerList();
+        // List<Answer> aList = this.answerRepository.findAll();
+        // q.setAnswerList(aList);
+        // Answer ans = aList.get(0);
+        // ans.setQuestion(q);
+        // this.answerRepository.save(ans);
+        // this.questionRepository.save(q);
 
-        assertEquals(1, alist.size());
-        assertEquals("ap메이지만 하는 틀딱이라서", alist.get(0).getContent());
+        this.questionRepository.delete(q);
+
 	}
 
 }
